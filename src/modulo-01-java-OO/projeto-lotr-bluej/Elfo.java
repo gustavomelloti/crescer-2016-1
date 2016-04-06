d0a1ca10f24c3328e5e35490a3c83d7a2ec58993
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-04-06 15:18:38 -0300

    refactor(core): remove o método atirarFlecha, incrementando suas funcionalides ao método atirarFlechaEmDwarf  .

--- src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
@@ -2,18 +2,15 @@ public class Elfo {
     private String nome;
     private int experiencia, flechas = 42, danoAtaque = 10;
     
-    public Elfo(String nome) {
+    public Elfo(String nome)
+    {
         this.nome = nome;
-    }
-    
-    public void atirarFlecha() {
-        experiencia++;
-        flechas--;
     }
     
     public void atirarFlechaEmDwarf(Dwarf d)
     {
-        this.atirarFlecha();
+        experiencia++;
+        flechas--;
         d.receberFlechada();
     }
 }
