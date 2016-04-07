aec0300bf695acab35552b02e23f29ff157182c9
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-04-07 15:08:55 -0300

    test(core): ajustar casos de testes para a nova lógica.

--- src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEraTest.java
@@ -6,44 +6,23 @@ import org.junit.Test;
 public class DataTerceiraEraTest
 {
     @Test
-    public void testarComAno2016()
+    public void testarComAno2016QueEhBissexto()
     {
         DataTerceiraEra ano2016 = new DataTerceiraEra(1, 10, 2016);
         assertTrue(ano2016.ehBissexto());
     }
     
     @Test
-    public void testarComAno4()
-    {
-        DataTerceiraEra ano4 = new DataTerceiraEra(1, 10, 4);
-        assertTrue(ano4.ehBissexto());
-    }
-    
-    @Test
-    public void testarComAno3024()
+    public void testarComAno3024QueNaoEhBissexto()
     {
         DataTerceiraEra ano3024 = new DataTerceiraEra(1, 10, 3024);
-        assertTrue(ano3024.ehBissexto());
+        assertFalse(ano3024.ehBissexto());
     }
     
     @Test
-    public void testarComAno2015()
+    public void testarComAno2015QueNaoEhBissexto()
     {
         DataTerceiraEra ano2015 = new DataTerceiraEra(1, 10, 2015);
         assertFalse(ano2015.ehBissexto());
-    }
-    
-    @Test
-    public void testarComAnoNegativo()
-    {
-        DataTerceiraEra anoNegativo = new DataTerceiraEra(1, 10, -20);
-        assertFalse(anoNegativo.ehBissexto());
-    }
-    
-    @Test
-    public void testarComAnoZero()
-    {
-        DataTerceiraEra anoZero = new DataTerceiraEra(1, 10, 0);
-        assertFalse(anoZero.ehBissexto());
     }
 }
