513b27f788daa1ecd457469b6f0b259b8bba90b3
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-04-07 17:36:10 -0300

    test(core): criação da classe de teste de inventário.

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class InventarioTest
+{   
+    @Test
+    public void testarInsercaoDeItem()
+    {
+        
+    }
+}
