ff87a28690d9ad925f6a70f3028050fe25297e75
Gustavo <gustavomelloti@gmail.com>
2016-04-07 20:41:11 -0300

    test(core): adicionar métodos de teste da classe inventário.

--- src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -7,8 +7,41 @@ import org.junit.Test;
 public class InventarioTest
 {   
     @Test
-    public void testarInsercaoDeItem()
+    public void testarInsercaoDeUmItem()
     {
+        Inventario i = new Inventario();
+        Item armadura = new Item(2, "Armadura X");
         
+        i.adicionarItem(armadura);
+        
+        assertSame(armadura, i.getItens().get(0));
+    }
+    
+    public void testarRemocaoDeItem()
+    {
+        Inventario i = new Inventario();
+        Item armadura = new Item(2, "Armadura X");
+        Item bota = new Item(2, "bota X");
+       
+        i.adicionarItem(armadura);
+        i.adicionarItem(bota);
+        i.removerItem(armadura);
+        
+        assertFalse(i.getItens().contains(armadura));
+    }
+    
+    public void testarInsercaoDe3Itens()
+    {
+        Inventario i = new Inventario();
+        
+        Item armadura = new Item(2, "Armadura X");
+        Item bota = new Item(2, "bota X");
+        Item capacete = new Item(2, "Capacete X");
+        
+        i.adicionarItem(armadura);
+        i.adicionarItem(bota);
+        i.adicionarItem(capacete);
+
+        assertTrue(i.getItens().size() == 3);
     }
 }
