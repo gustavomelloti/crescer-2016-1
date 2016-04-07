131fabcde373d622014c36fe1c9036d2849311e8
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-04-07 15:46:09 -0300

    feat(core): implementar funcionalidade que o dwarf morre.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -2,16 +2,21 @@ public class Dwarf
 {
     private int qtdVida;
     private String nome;
+    private Status status;
     
     public Dwarf(String nome)
     {
         this.nome    = nome;
         this.qtdVida = 110;
+        this.status  = Status.VIVO;
     }
     
     public void receberFlechada()
     {
         this.qtdVida -= 10;
+        
+        if (this.qtdVida == 0)
+            this.status = Status.MORTO;
     }
     
     public void setNome(String nome) 
@@ -27,5 +32,10 @@ public class Dwarf
     public int getQtdVida()
     {
         return this.qtdVida;
+    }
+    
+    public Status getStatus()
+    {
+        return this.status;
     }
 }
