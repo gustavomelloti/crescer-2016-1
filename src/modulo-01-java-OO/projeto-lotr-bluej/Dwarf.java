1f27375f10d8028e58a051d457878a02b4966a2f
Gustavo <gustavomelloti@gmail.com>
2016-04-07 21:30:26 -0300

    feat(core): implementar o método de sortear número da sorte. adiciona o atributo experiencia. altera a lógica do metodo receberFlechada.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -1,11 +1,11 @@
 public class Dwarf
 {
-    private int qtdVida = 110;
+    private int qtdVida = 110, experiencia = 0;
     private String nome;
     private Status status = Status.VIVO;
     private Inventario inventario = new Inventario();
     private DataTerceiraEra dataNascimento;
-    
+   
     public Dwarf(String nome)
     {
         this.nome = nome;
@@ -20,7 +20,14 @@ public class Dwarf
     
     public void receberFlechada()
     {
-        if (this.qtdVida >= 10)
+        double numeroSorte = this.getNumeroSorte();
+        
+        if (numeroSorte < 0) {
+            experiencia+=2;
+            return;
+        }
+        
+        if (numeroSorte > 100 && this.qtdVida >= 10)
             this.qtdVida -= 10;
         
         if (this.qtdVida == 0)
@@ -35,6 +42,19 @@ public class Dwarf
     public void perderItem(Item item)
     {
         this.inventario.removerItem(item);
+    }
+    
+    public double getNumeroSorte ()
+    {   
+        double numeroASerRetornado = 101;
+        
+        if (this.dataNascimento.ehBissexto() && this.qtdVida >= 80 && this.qtdVida <= 90)
+             numeroASerRetornado*= -33;
+             
+        if (!this.dataNascimento.ehBissexto() && (this.nome.equals("Seixas") || this.nome.equals("Meireles")))
+            numeroASerRetornado = (numeroASerRetornado * 33) % 100;
+
+        return numeroASerRetornado;
     }
     
     public void setNome(String nome) 
