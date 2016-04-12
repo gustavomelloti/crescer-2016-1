b8eb043c2e061b0089c1eed7cae4b7ad5cf97da4
gustavomelloti <gustavomelloti@gmail.com>
2016-04-12 10:00:30 -0300

    Update ElfoNoturno.java
    fix(core): corrigir cálculo de perder vida ao atirar flecha.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturno.java
@@ -15,7 +15,7 @@ public class ElfoNoturno extends Elfo
         super.atirarFlechaEmDwarf(d);
         this.experiencia+= 2;
         
-        double qtdVidaAposAtirarFlecha = this.qtdVida - 5;
+        int qtdVidaAposAtirarFlecha = (int) this.qtdVida - this.qtdVida * 0.05;
         
         if (qtdVidaAposAtirarFlecha <= 0)
             this.status = Status.MORTO;
