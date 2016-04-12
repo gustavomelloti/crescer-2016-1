a78716b58cffe1aae39665dd3f852719d4bdced5
Gustavo <gustavomelloti@gmail.com>
2016-04-12 00:20:49 -0300

    teste(core): adicionar cenários de teste de inventário na classe elfoverde.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoVerdeTest.java
@@ -15,7 +15,7 @@ public class ElfoVerdeTest
    }
    
    @Test
-   public void adicionarItemEspadadeacovaliriano()
+   public void adicionarItemEspadaDeAcoValiriano()
    {
        ElfoVerde elfoV = new ElfoVerde("Gustavo");
        Item espada = new Item(10, "Espada de aço valiriano");
@@ -30,6 +30,41 @@ public class ElfoVerdeTest
        Item arco = new Item(10, "Arco e Flecha de Vidro");
        elfoV.adicionarItem(arco);
        assertTrue(elfoV.getInventario().getItens().contains(arco));
+   }
+   
+   @Test
+   public void adicionarItemArmaduraEEspadaDeAcoValiriano()
+   {
+       ElfoVerde elfoV = new ElfoVerde("Gustavo");
+       Item espada = new Item(10, "Espada de aço valiriano");
+       Item armadura = new Item(10, "Armadura X");
+       elfoV.adicionarItem(armadura);
+       elfoV.adicionarItem(espada);
+       assertTrue(espada == elfoV.getInventario().getItens().get(0) && elfoV.getInventario().getItens().size() == 1);
+   }
+   
+   @Test
+   public void adicionarItemArmaduraEArcoeFlechadeVidro()
+   {
+       ElfoVerde elfoV = new ElfoVerde("Gustavo");
+       Item arco = new Item(10, "Arco e Flecha de Vidro");
+       Item armadura = new Item(10, "Armadura X");
+       elfoV.adicionarItem(armadura);
+       elfoV.adicionarItem(arco);
+       assertTrue(arco == elfoV.getInventario().getItens().get(0) && elfoV.getInventario().getItens().size() == 1);
+   }
+   
+   @Test
+   public void adicionarItensPermitidosEArmadura()
+   {
+       ElfoVerde elfoV = new ElfoVerde("Gustavo");
+       Item arco = new Item(10, "Arco e Flecha de Vidro");
+       Item armadura = new Item(10, "Armadura X");
+       Item espada = new Item(10, "Espada de aço valiriano");
+       elfoV.adicionarItem(arco);
+       elfoV.adicionarItem(armadura);
+       elfoV.adicionarItem(espada);
+       assertTrue(arco == elfoV.getInventario().getItens().get(0) && espada == elfoV.getInventario().getItens().get(1) && elfoV.getInventario().getItens().size() == 2);
    }
    
    @Test
