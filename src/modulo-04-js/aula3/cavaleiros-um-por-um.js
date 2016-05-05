c21206da1d27b75121a77e980bc8b1e59e9472c7
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-05 14:46:31 -0300

    fix(js): corrigir listagem um por um após correção.

--- src/modulo-04-js/aula3/cavaleiros-um-por-um.js
+++ src/modulo-04-js/aula3/cavaleiros-um-por-um.js
@@ -1,11 +1,29 @@
-$(document).ready(function(){
-    for (var i in goldSaints)
-        exibirCavaleiros(i);
-    
-    function exibirCavaleiros(index) {
-        setTimeout(function() {
-            $div = $('<div>').append($('<img>').attr('src', goldSaints[index].imagens[0].url));
-            $(".cavaleiros").append($div.append($div));
-        }, 1000*index);
-    }
-});
+$(function() {
+  // Carregando imagens sequencialmente na ordem, de forma recursiva.
+  // No carregamento já registramos o evento de click na imagem para exibir os detalhes do cavaleiro.
+
+  (function carregaImg(indice) {
+    var $detalhesCavaleiro = $('#detalhes-cavaleiro');
+    var cavaleiro = goldSaints[indice];
+    var imgCavaleiro = new Image();
+    imgCavaleiro.src = cavaleiro.imagens[0].url;
+    imgCavaleiro.alt = cavaleiro.nome;
+    imgCavaleiro.id = cavaleiro.id;
+    imgCavaleiro.onload = function() {
+      var $img = $(imgCavaleiro);
+      $img.appendTo($('<li>').appendTo($('#cavaleiros'))).fadeIn();
+      $img.click(function() {
+        var self = $(this);
+        var nome = self.attr('alt');
+        var altura = goldSaints.filter(function(elem) {
+          return elem.id === parseInt(self.attr('id'));
+        })[0].alturaCm;
+        $detalhesCavaleiro.children().detach();
+        $detalhesCavaleiro.append($('<h3>').text( nome ));
+        $detalhesCavaleiro.append($('<h3>').text( altura / 100 ));
+      });
+      if (indice < goldSaints.length - 1) carregaImg(indice + 1);
+    };
+  })(0);
+
+  });
