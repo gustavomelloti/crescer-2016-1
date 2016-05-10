dfae351b08600dc46e84d82d6be62587287134eb
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-10 17:29:33 -0300

    feat(c#): publicar exercício MVC incompleto.

---
+++ src/modulo-05-c#/MVC/MVC/Controllers/StreetFighterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class StreetFighterController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SobreMim()
+        { 
+            return View();
+        }
+    }
+}
---
+++ src/modulo-05-c#/MVC/MVC/Models/SobreMimModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMimModel
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Foto { get; set; }
+    }
+}
