7aa1a5e5bffbdbf31f15704ee95dc8bbb43dd9d0
Gustavo <gustavomelloti@gmail.com>
2016-05-11 01:06:41 -0300

    feat(c#): publicar resolução do desafio de carregar css, com caminho estático.

---
+++ src/modulo-05-c#/MVC/MVC/Controllers/DesafioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC.Controllers   
+{
+    public class DesafioController : Controller
+    {
+        public ActionResult Index(string aluno)
+        {
+            ViewBag.aluno = aluno;
+
+            if (verificarSeArquivoExiste(aluno))
+                return View();
+            else
+                return RedirectToAction("NaoEncontrado");
+        }
+
+        public ActionResult NaoEncontrado()
+        {
+            return View();
+        }
+
+        private Boolean verificarSeArquivoExiste(string arquivo)
+        {
+            return System.IO.File.Exists(String.Format(@"C:\Users\Gustavo\Documents\crescer-2016-1\src\modulo-05-c#\MVC\MVC\Content\Estilos\{0}.css", arquivo));
+        }
+    }
+}
