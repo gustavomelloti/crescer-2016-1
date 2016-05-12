560b8317c29d0e89262753bf12a92da13eed08ce
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-12 17:35:18 -0300

    feat(LojaNinja): publicar projeto base, e adicionar função de cadastras.

---
+++ src/modulo-05-c#/LojaNinja/LojaNinja.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,73 @@
+﻿using LojaNinja.Dominio;
+using LojaNinja.MVC.Models;
+using LojaNinja.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private RepositorioVendas repositorio = new RepositorioVendas();
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Salvar(PedidoModel model)
+        {
+            if (model.Estado == "RS" && model.Cidade == "SL")
+                ModelState.AddModelError("", "Cidade e Estado inválidos");
+
+            if (model.DataEntrega.AddDays(-7) < DateTime.Today)
+                ModelState.AddModelError("DataEntrega", "Data deve ser maior do que 7 dias");
+
+            if (ModelState.IsValid)
+            {
+                Pedido novoPedido; 
+
+                try
+                {
+                    novoPedido = new Pedido(model.DataEntrega, model.NomeProduto, model.Valor, model.TipoPagamento, model.NomeCliente, model.Cidade, model.Estado);
+                    repositorio.IncluirPedido(novoPedido);
+                } 
+                catch(ArgumentException ex)
+                {
+                    ModelState.AddModelError("", ex.Message);
+                    return View("Cadastro", model);
+                }
+
+                return View("Detalhes", novoPedido);
+            }
+            else
+            {
+                return View("Cadastro", model);
+            }
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var pedido = repositorio.ObterPedidoPorId(id);
+
+            return View(pedido);
+        }
+
+        public ActionResult Listagem()
+        {
+            var pedidos = repositorio.ObterPedidos();
+
+            return View(pedidos);
+        }
+
+        public ActionResult Deletar(int id)
+        {
+            repositorio.ExcluirPedido(id);
+
+            return View();
+        }
+    }
+}
--- src/modulo-05-c#/LojaNinja/LojaNinja/scripts/cadastro.js
+++
@@ -1,5 +0,0 @@
-﻿$(document).ready(function () {
-    $('#Estado').mask('aa');
-    $('#DataDesejoEntrega').datepicker({ minDate: '0', dateFormat: 'dd/mm/yy' });
-    $('#ValorVenda').maskMoney({showSymbol:true, symbol:"R$", decimal:",", thousands:"."});
-});
