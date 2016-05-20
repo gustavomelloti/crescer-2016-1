a2209ce16a9976a32d4ccfc9b7ae99b0026c534b
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-20 15:05:15 -0300

    feat(ajax): publicar projetos atualizados.

---
+++ src/modulo-06-ajax/dia1/Exercicios-Spotify/Exercicios-Spotify/Scripts/custom/Domain.js
@@ -0,0 +1,21 @@
+﻿var aplicarRegraJustinBiber = function () {
+    var numeroEntre1E10 = Math.random() * (10 - 1) + 1;
+
+    return numeroEntre1E10 <= 8 ? '' : 'Justin Bieber';
+};
+
+var adicionarImagensAlbuns = function (albuns) {
+    limparDadosUltimaPesquisa();
+
+    if (albuns.items == 0)
+        $('#erro-albuns').html("Nenhum álbum encontrado.");
+
+    albuns.items.forEach(function (element) {
+        $('#albuns').append($('<li>').append($('<img>').attr('src', element.images[1].url)));
+    });
+}
+
+var limparDadosUltimaPesquisa = function () {
+    $("#albuns").html("");
+    $('#erro-albuns').html("");
+}
