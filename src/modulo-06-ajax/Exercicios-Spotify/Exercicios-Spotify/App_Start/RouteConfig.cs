1f45e2b5856122a6337286799dfc3511b21131b7
Gustavo <gustavomelloti@gmail.com>
2016-05-20 00:18:25 -0300

    feat(ajax): definindo rota padrão como Spotify/DonzelaDeFerro.

--- src/modulo-06-ajax/Exercicios-Spotify/Exercicios-Spotify/App_Start/RouteConfig.cs
+++ src/modulo-06-ajax/Exercicios-Spotify/Exercicios-Spotify/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Exercicios_Spotify
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Spotify", action = "DonzelaDeFerro", id = UrlParameter.Optional }
             );
         }
     }
