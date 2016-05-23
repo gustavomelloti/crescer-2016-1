fac8c049bc163356510b2371fc6d5ec4f46aee86
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-05-23 16:12:54 -0300

    feat(ajax): fazer funções auxiliares no javascript.

---
+++ src/modulo-06-ajax/dia2/CdZ/src/CdZ.MVC/Scripts/Custom/HelperFormat.js
@@ -0,0 +1,11 @@
+﻿Number.prototype.converterParaMetros = function () {
+    return this / 100;
+};
+
+Number.prototype.lbParaKilos = function () {
+    return this / 2.20462262;
+};
+
+Number.prototype.formatarNumero = function () {
+    return Math.round(this * 100) / 100;
+}
