9f1a474e4d678ade70deeca48a66daaaea95a54f
Gustavo <gustavomelloti@gmail.com>
2016-05-22 21:47:26 -0300

    fix(ajax): ajustar abertura da dialog de detalhes.

--- src/modulo-06-ajax/dia2/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Listagem.js
+++ src/modulo-06-ajax/dia2/CdZ/src/CdZ.MVC/Scripts/Views/Cavaleiro/Listagem.js
@@ -1,35 +1,35 @@
 ﻿'use strict';
 
-function abrirModalDetalhesCavaleiro() {
-    var idCavaleiro = $(this).parents('li:first').attr('data-id-cavaleiro');
-
-    buscarCavaleiroPorId(idCavaleiro).then(
-        function onSuccess(res) {
-            console.log(res);
-            $('#d_id').html(res.data.Id);
-            $('#d_nome').html(res.data.Nome);
-            $('#d_peso').html(res.data.PesoLb);
-            $('#d_altura').html(res.data.AlturaCm);
-            $('#d_nascimento').html(res.data.DataNascimento);
-            $('#d_signo').html(res.data.Signo);
-            $('#d_tipo_sanguineo').html(res.data.TipoSanguineo);
-            $('#d_local_nascimento').html(res.data.LocalNascimento);
-            $('#d_local_treinamento').html(res.data.LocalTreinameno);
-            $('#d_golpes').html();
-            $('#d_imagens').html();
-            $('#dialog').dialog({});
-        },
-        function onError(res) {
-            alert("Erro ao buscar detalhes do cavaleiro");
-        }
-    );
-}
-
 function buscarCavaleiroPorId(id) {
     return $.ajax({ url: urlCavaleiroGetId, type: 'GET', data: { id: id } });
 }
 
 $(function () {
+    $(document).on("click", ".info", function () {
+        var idCavaleiro = $(this).parents('li:first').attr('data-id-cavaleiro');
+        buscarCavaleiroPorId(idCavaleiro).then(
+            function onSuccess(res) {
+                $('#d_id').html(res.data.Id);
+                $('#d_nome').html(res.data.Nome);
+                $('#d_peso').html(res.data.PesoLb);
+                $('#d_altura').html(res.data.AlturaCm);
+                $('#d_nascimento').html(res.data.DataNascimento);
+                $('#d_signo').html(res.data.Signo);
+                $('#d_tipo_sanguineo').html(res.data.TipoSanguineo);
+                $('#d_local_nascimento').html(res.data.LocalNascimento);
+                $('#d_local_treinamento').html(res.data.LocalTreinameno);
+                $('#d_golpes').html();
+                $('#d_imagens').html();
+                $('#dialog').dialog({});
+            },
+            function onError(res) {
+                alert("Erro ao buscar detalhes do cavaleiro");
+            }
+        );
+    });
+
+
+
     $(document).on("click", "#contentPager a[href]", function () {
         $.ajax({
             url: $(this).attr("href"),
