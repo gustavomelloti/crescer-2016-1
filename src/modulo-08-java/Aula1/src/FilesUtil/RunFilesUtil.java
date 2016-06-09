cd4d309ed705d7223793b08364b159c1de9464cb
Gustavo Silva de Mello <gustavo.mello@cwinet.local>
2016-06-09 13:15:13 -0300

    feat(java): adicionar tratamento de erros adicionais na classe RunFilesUtil.

--- src/modulo-08-java/Aula1/src/FilesUtil/RunFilesUtil.java
+++ src/modulo-08-java/Aula1/src/FilesUtil/RunFilesUtil.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class RunFilesUtil {
-    public static void main(String[] args) {
-        System.out.println("Bem-vindo ao Crescer-Linux. Para encerrar o terminal digite. (PONTO FINAL)");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bem-vindo ao Crescer-Linux. Para encerrar o terminal digite . (PONTO FINAL)");
         
         try {
             MeuFileUtils filesUtils = new MeuFileUtils();
@@ -27,6 +27,8 @@ public class RunFilesUtil {
                     case "mv":
                         filesUtils.mover(comandoEmArray[1], comandoEmArray[2]);
                     break;
+                    default:
+                        throw new Exception();
                 }
                 comando = c.nextLine();
             }
@@ -36,6 +38,10 @@ public class RunFilesUtil {
             System.out.print("Você deve informar um arquivo, não um diretório.");
         } catch (IOException e) {
             System.out.print("Erro ao realizar operação.");
+        } catch (NullPointerException e) {
+            System.out.print("Comando com informações insuficientes.");
+        } catch(Exception e ) {
+            System.out.print("Comando desconhecido.");
         }
     }
 }
